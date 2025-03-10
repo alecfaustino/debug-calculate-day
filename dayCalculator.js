0699549854d2bbe18b5b9b124389b9df9bc0d6c5
Alec Faustino <alecfaustino@outlook.com>
2025-03-09 20:02:32 -0600

    all tests pass and refactored

--- dayCalculator.js
+++ dayCalculator.js
@@ -1,42 +1,52 @@
 function calculateDayInYear(date) {
-  const splitDate = date.split('/');
+  const splitDate = date.split('/'); // is an array of the input
+  // the format is YYYY/MM/DD
   const year = Number(splitDate[0]);
   const month = Number(splitDate[1]);
   const day = Number(splitDate[2]);
 
+  // will check if a number like 13 is inputted
+  // was correctly implemented
   const validMonth = function(month) {
-    return month && month >= 1 && month < 12;
-  }
-
-  const validDay = function(month, day) {
-    return day && day >= 1 && day < DAYS_IN_MONTH[month - 1];
-  }
-
-  const calculateDayNumber = function(month, day) {
-    let dayOfYear = 1;
-
-    for (let i = 1; i < month; i++) {
-      dayOfYear += DAYS_IN_MONTH[i - 1];
-    }
-
-    return dayOfYear;
-  }
-
-  const daysInFeb = function(year) {
-    return 28;
+    return month && month >= 1 && month <= 12;
   }
 
   const isLeapYear = function(year) {
-    return isMultiple(year, 400) || !isMultiple(year, 100) && isMultiple(year, 4);
+    return ((isMultiple(year, 4) && !isMultiple(year, 100))) || (isMultiple(year, 400));
+  }
+  
+  const daysInFeb = function(year) {
+    // if it is, 29. if it is not, 28.
+    return isLeapYear(year) ? 29 : 28;
   }
 
   const DAYS_IN_MONTH = [31, daysInFeb(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+  // now fixed
+  const validDay = function(month, day) {
+    return day && day >= 1 && day <= DAYS_IN_MONTH[month - 1]; // month - 1 because 0 indexed array. 
+  }
+
+  
+  
+  const calculateDayNumber = function(month, day) {
+    // should automatically count the days we 
+    let dayOfYear = day;
+    
+    // looping through 
+    //January is DAYS_IN_MONTH[0]
+    for (let i = 1; i < month; i++) {
+      dayOfYear += DAYS_IN_MONTH[i - 1];
+    }
+    
+    return dayOfYear;
+  }
+
   if (year && validMonth(month) && validDay(month, day)) {
-    console.log(date, "is day", calculateDayNumber(month, day), "of the year", year);
+    // console.log(date, "is day", calculateDayNumber(month, day), "of the year", year);
     return calculateDayNumber(month, day);
   } else {
-    console.log("Invalid date");
+    // console.log("Invalid date");
   }
 }
 
